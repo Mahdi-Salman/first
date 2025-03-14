3b21bd81fb5c33376af603d5ac83ca0f4c12ca5e
mahdi <mahdisalman033@gmail.com>
2025-03-15 02:05:19 +0330

    Create user.sql

---
+++ user.sql
@@ -0,0 +1,10 @@
+CREATE TABLE user(
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(100),
+  main_role ENUM('passenger', 'admin'),
+  hometown VARCHAR(50),
+    password VARCHAR(100),
+    login_date TIMESTAMP,
+    account_status ENUM('active', 'deactvie')    
+);
