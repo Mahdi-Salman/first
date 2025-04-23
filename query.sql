c47168a2f887e86939ee4e90d2d6835920361582
mahdi <mahdisalman033@gmail.com>
2025-04-23 12:05:42 +0330

    Delete query.sql

--- query.sql
+++
@@ -1,4 +0,0 @@
-SELECT DISTINCT user.first_name, user.last_name
-FROM user
-LEFT JOIN reservation ON user.user_id = reservation.user_id
-WHERE reservation.status != 'paid' OR reservation.user_id IS NULL;
